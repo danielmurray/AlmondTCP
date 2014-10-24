0562b9a34fd676b56aeb9b998d20c1a42e3cb69a
Dan <daniel.phillip.murray@gmail.com>
2014-10-24 00:08:46 -0500

    First commit

---
+++ devices.js
@@ -0,0 +1,63 @@
+var fs = require("fs");
+var sys = require('sys')
+var util = require('util');
+var exec = require('child_process').exec;
+
+var EventEmitter = require('events').EventEmitter;
+
+
+var Devices = function() {
+    EventEmitter.call(this);
+
+	that = this;
+	this.devicesJson = {};
+	this.devices = []
+        	
+	var listen = function(){
+		fs.watch('/DeviceList.xml', function (curr, prev) {                                      
+			that.fetchDevices();
+		});
+	}
+	    	
+	var fetchDevices = function(){
+		exec("/www/setdevice cmd=list",function(error, stdout, stderr){
+			that.updateDevices(stdout);
+		})
+	}
+
+	var updateDevices = function(devicesJson){
+		this.devicesJson = JSON.parse(devicesJson)
+		for( var key in this.devicesJson){
+			var deviceJson = this.devicesJson[key]
+			var device = this.getDevice(deviceJson.DeviceID)
+			if( !device )
+				
+
+		}
+		this.emit('change', this.devices)
+	}
+
+	this.getDevice = function(id){
+		for( var i = 0; i < this.devices.length; i++){
+			var device = this.devices[i]
+			if (device.DeviceID == id)
+				return device
+		}
+	}
+
+	this.getDevices = function(){
+
+	}
+	
+	listen()
+}
+util.inherits(Devices, EventEmitter);
+
+var emitFunction = function(devicesJson){
+	//console.log(devicesJson)
+	console.log('Device Update!')
+	
+}
+
+var devices = new Devices();
+devices.on('change', emitFunction)
---
+++ sync.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+LOCALFOLDER='almondroot'
+REMOTEIP='10.10.10.254'
+REMOTEUSER='root'
+REMOTEPASS='root'
+
+shopt -s globstar
+FILES=$LOCALFOLDER/**
+
+function pathToArray {
+	PATHARRAY=()
+	IFS='/' read -ra ADDR <<< "$1"
+	for i in "${ADDR[@]}"; do
+	    PATHARRAY+=($i)
+	done
+}
+
+function buildDir {
+	pathToArray $2
+	PATHBUILDCOMMAND="cd / && "
+	BUILTPATH=""
+	for (( i=0; i<${#PATHARRAY[@]}-1; i++ ))
+	do
+	   PATHBUILDCOMMAND+="cd ${PATHARRAY[i]} || (mkdir ${PATHARRAY[i]} && cd ${PATHARRAY[i]}) &&"
+	   BUILTPATH+="${PATHARRAY[i]}/"
+	done
+	PATHBUILDCOMMAND+="echo \"Just Built $BUILTPATH\""
+	sshpass -p $REMOTEPASS ssh $REMOTEUSER@$REMOTEIP "$PATHBUILDCOMMAND"
+	sshpass -p $REMOTEPASS scp -r $1 "$REMOTEUSER@$REMOTEIP:/$2"
+}
+
+function update {
+	sshpass -p $REMOTEPASS scp -r $1 "$REMOTEUSER@$REMOTEIP:/$2"  || buildDir "$1" "$2"
+}
+
+function cdRoot {
+	REMOTEPATH=$(expr $1 : '[^/]*/\(.*\)')
+}
+
+function updateFileDirectory {
+	for LOCALPATH in $(find $LOCALFOLDER -type f | grep -v '/\.')
+	do
+		cdRoot "$LOCALPATH"
+		CHANGES=$(sshpass -p $REMOTEPASS ssh $REMOTEUSER@$REMOTEIP "cat /$REMOTEPATH" | diff - $LOCALPATH)
+		if [ "$CHANGES" != "" ] 
+		then
+			echo "$CHANGES"
+			echo "-----------------"
+		    update "$LOCALPATH" "$REMOTEPATH"
+		fi
+	done
+}
+
+function syncFileDirectory {
+	inotifywait -m -r --format '%w%f %f' -e modify -e move -e create -e delete $LOCALFOLDER | while read LOCALPATH LOCALFILE
+	do
+		if [[ $LOCALFILE != .* ]] 
+		then
+			cdRoot "$LOCALPATH"
+			update "$LOCALPATH" "$REMOTEPATH"
+			echo "$REMOTEPATH"
+			echo "-----------------"
+		fi
+	done
+}
+
+count=$( ping -w 1 -c 1 $REMOTEIP | grep icmp* | wc -l )
+
+if [ $count -eq 0 ]
+then
+    echo "No Almond, go to localhost:8000 to view files"
+    cd www
+    python -m SimpleHTTPServer
+else
+	updateFileDirectory
+	syncFileDirectory
+fi
